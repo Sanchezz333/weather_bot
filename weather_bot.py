9e48292669cfe5a75dd88493b765867174375d14
AlexSh <sanchezz@AlexShs-MacBook-Pro.local>
2022-09-07 13:18:05 +0300

    add weather message for dates

--- weather_bot.py
+++ weather_bot.py
@@ -74,11 +74,10 @@ def list_of_days():
     return keys
 
 def get_weather_text(weather_data, city, day):
-    text = ''
+    text = f'В городе {city} на {day.strftime('%d-%m-%Y')}:\n'
     for i in weather_data['list']:
         if day.strftime('%Y-%m-%d') in i['dt_txt']:
-            text += f"""В городе {city} на {day.strftime('%d-%m-%Y')}:
-В {datetime.utcfromtimestamp(i['dt']).strftime('%H:%M')}
+            text += f"""В {datetime.utcfromtimestamp(i['dt']).strftime('%H:%M')}
 Температура: {i['main']['temp']} градусов
 Ощущается как {i["main"]["feels_like"]} градусов
 Влажность {i["main"]["humidity"]}%
